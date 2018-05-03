ce503b0c838118d44a889caea1fdd3b38df6771b
Jason Scheirer <jason.scheirer@gmail.com>
2018-05-03 10:01:43 -0700

    Now we need Bolt, because of Bolt

--- world.go
+++ world.go
@@ -1,5 +1,7 @@
 package mud
 
+import bolt "github.com/coreos/bbolt"
+
 // World represents a gameplay world. It should keep track of the map,
 // entities in the map, and players.
 type World interface {
@@ -8,6 +10,7 @@ type World interface {
 
 type dbWorld struct {
 	filename string
+	database *bolt.DB
 }
 
 // GetDimensions returns the size of the world
@@ -15,8 +18,19 @@ func (w *dbWorld) GetDimensions() (uint32, uint32) {
 	return uint32(1 << 31), uint32(1 << 31)
 }
 
+func (w *dbWorld) load() {
+	db, err := bolt.Open(w.filename, 0x600, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	w.database = db
+}
+
 // LoadWorldFromDB will set up an on-disk based world
 func LoadWorldFromDB(filename string) World {
 	newWorld := dbWorld{filename: filename}
+	newWorld.load()
 	return &newWorld
 }
