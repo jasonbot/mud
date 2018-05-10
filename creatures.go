5350943db2563c2bd753f07a038d9a087511e3da
Jason Scheirer <jason.scheirer@gmail.com>
2018-05-10 13:54:41 -0700

    Seconds in refresh

--- creatures.go
+++ creatures.go
@@ -17,7 +17,7 @@ type Attack struct {
 	AP       uint64   `json:""`
 	RP       uint64   `json:""`
 	Effects  []string `json:""`
-	Recovery uint64   `json:""` // In Milliseconds
+	Recovery uint64   `json:""` // In Seconds
 }
 
 // CreatureType is the type of creature (Hostile: true is monster, false is NPC)
