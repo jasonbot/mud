c680673f4054075967a818dd44dd3f0af2368bc5
Jason Scheirer <jason.scheirer@gmail.com>
2018-05-13 12:07:20 -0700

    Timestermp zippses

--- make-packages.sh
+++ make-packages.sh
@@ -2,6 +2,7 @@
 
 OSSES="darwin linux windows"
 ARCHES="amd64"
+ZIPPREFIX=`date +%Y-%m-%d`
 
 for OS in $OSSES
 do
@@ -12,7 +13,7 @@ do
 		mkdir $OUTDIRNAME
 		for file in cmd/*.go
 		do
-		fn=${file##*/}
+			fn=${file##*/}
 			OUTITEM=$OUTDIRNAME/${fn%%.go}
 			if [ "z$OS" = "zwindows" ]
 			then
@@ -21,7 +22,12 @@ do
 			GOOS=$OS GOARCH=$ARCH go build -o $OUTITEM $file
 		done
 		cp *.json *.txt $OUTDIRNAME
-		zip $OUTDIRNAME.zip $OUTDIRNAME/*
+		if [ "z$OS" = "zwindows" ]
+		then
+			zip mud-$ZIPPREFIX-$OS-$ARCH.zip $OUTDIRNAME/*
+		else
+			tar cvzf mud-$ZIPPREFIX-$OS-$ARCH.tgz $OUTDIRNAME
+		fi
 		rm -rf $OUTDIRNAME
 	done
 done
